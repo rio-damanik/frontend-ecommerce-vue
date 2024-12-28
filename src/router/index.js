9a5d0b8051b3dff263a9893d12a79557c0837ba5
rio-damanik <rio71385@gmail.com>
2024-12-28 11:09:25 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },             
+  {
+    path: '/men',
+    name: 'men',
+    component: () => import('../views/MenView.vue')
+  },
+  {
+    path: '/women',
+    name: 'women',
+    component: () => import('../views/WomenView.vue')
+  },
+  {
+    path: '/kids',
+    name: 'kids',
+    component: () => import('../views/KidsView.vue')
   }
 ]
 
